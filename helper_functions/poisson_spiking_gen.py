4d0966ab970cb4dfe2af342d6f941c364003be2c
Ole Richter <git@olerichter.com>
2020-11-22 18:27:05 +0100

    added positive and negaive correlation, saidly somehow the indent got all messed up

--- helper_functions/poisson_spiking_gen.py
+++ helper_functions/poisson_spiking_gen.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 @author: wgirao
+@author: o.j.richter@rug.nl
 @based-on: asonntag
 """
 
 # Creates the arrays containing the spiking times for both pre- and postsynaptic neurons.
 """
 input:
+ - rate_pre: pre synaptic rate in Hz
+ - rate_post: post synaptic rate in Hz
+ - t_run: time steps to run
+ - dt: time step in sec
+ - noise: noise multipier for low_freq ( max/min of uniform distribution is 1/high_feq*noise)
+ - seed: default 0, the seed for the random generator, for reproducibility numpy version must match.
+ - correlation: random (default): uniform balanced correlation - zero centered, 
+                positive: only the positive shift of the random uniform distribution, 
+                negative: only the negative shift of the random uniform distribution.
 
 output:
 - pre_spikes_t (numpy array): pres- spikes times
@@ -15,15 +25,24 @@ output:
 Comments:
 """
 import numpy as np
-from brian2 import uniform, rand, size
-from remove_close_spikes import *
+import logging
 
-def poisson_spiking_gen(rate_pre, rate_post, t_run, dt, noise):
+
+# from remove_close_spikes import *
+
+
+def poisson_spiking_gen(rate_pre, rate_post, t_run, dt, noise, seed=0, correlation="random"):
 	# Initializing decision variables
 	rate_low = -1
 	rate_high = -1
-    
-    # Arrays with spike times
+
+	# Initialise random gen
+	# from seed to have reproducible results, numpy version must match!
+	seedsequ = np.random.SeedSequence(seed)
+	logging.info('the following random seed is used for spike generation: {}'.format(seedsequ.entropy))
+	randomgen = np.random.PCG64(seedsequ)
+
+	# Arrays with spike times
 	pre_spikes_t = np.array([])
 	post_spikes_t = np.array([])
 
@@ -42,47 +61,55 @@ def poisson_spiking_gen(rate_pre, rate_post, t_run, dt, noise):
 		# Array with each time step of simulation
 		times = np.arange(0, t_run, dt)
 
-		# random number (in [0,1)) for each time step (membrane threshold for spike gen?) 
-		vt_highrate = rand(size(times))
+		# random number (in [0,1)) for each time step (membrane threshold for spike gen?)
+		vt_highrate = randomgen.random(np.size(times))
 
 		# Indexes where spike happened
-		spikes_highrate = (spikes_per_s*dt) > vt_highrate
+		spikes_highrate = (spikes_per_s * dt) > vt_highrate
 
 		# Array containing the spike times of the neuron spiking at the higher rate.
 		"""
 		'np.argwhere(spikes_highrate)' is the index of the array 'vt_highrate' where the random number was smaller than '(spikes_per_s*dt)' (chance of spike in each time step);
 		"""
-		highrate_spikes_t = np.argwhere(spikes_highrate)*dt
+		highrate_spikes_t = np.argwhere(spikes_highrate) * dt
 
 		if rate_low == 0:
 			lowrate_spikes_t = np.array([])
 		elif rate_low > 0:
-			spike_prob = rate_low/rate_high
-			rand_num = rand(len(highrate_spikes_t))
+			spike_prob = rate_low / rate_high
+			rand_num = randomgen.random(len(highrate_spikes_t))
 			# Array containing the spike times of the neuron spiking at the lower rate.
 			"""
 			Only gets the values 'highrate_spikes_t[x]', where x is the index of 'rand_num' for which the value is smaller than 'spike_prob'.
 			"""
 			lowrate_spikes_t = highrate_spikes_t[rand_num < spike_prob]
 
-			# ATENTION - experiment with negative shift btween spike timings should be from here on.
-
-			avg_interval = 1.0/spikes_per_s
-
+			# @TODO we should think about not making the distribution adaptive, but fixed to 1-3* STDP time constant?
+			# be aware of impact on correlation
+			avg_interval = 1.0 / spikes_per_s
 			# 'len(lowrate_spikes_t)'s floats between ['-avg_interval*noise','avg_interval*noise')
-			shifts = uniform(-avg_interval*noise, avg_interval*noise, len(lowrate_spikes_t))
+			shifts = randomgen.uniform(-avg_interval * noise, avg_interval * noise, len(lowrate_spikes_t))
+			if correlation == "random":
+				pass
+			elif correlation == "positive":
+				shifts = np.abs(shifts)
+			elif correlation == "negative":
+				shifts = np.abs(shifts) * -1
+			else:
+				raise KeyError("correlation can only be: random (default), positive, negative")
 
-			# 'lowrate_spikes_t' has now some of the same spike times as 'highrate_spikes_t' but with a shifted value (for more or for less) 
-			lowrate_spikes_t = lowrate_spikes_t.flatten()+shifts
+			# 'lowrate_spikes_t' has now some of the same spike times as 'highrate_spikes_t' but with a shifted value (for more or for less)
+			lowrate_spikes_t = lowrate_spikes_t.flatten() + shifts
 
 			# Removing negative values
 			lowrate_spikes_t = lowrate_spikes_t[lowrate_spikes_t > 0]
 
-			# Removing spikes that are too close - QUESTION (why only with lowrate?)
-			lowrate_spikes_t = remove_close_spikes(np.sort(lowrate_spikes_t), dt, t_run)
+		# Removing spikes that are too close - QUESTION (why only with lowrate?) ANSWER: because of the added noise only to low rate
+		# I would not even remove close spikes because otherwise you will just have 2 excecuted shortly after one another
+		# lowrate_spikes_t = remove_close_spikes(np.sort(lowrate_spikes_t), dt, t_run)
 
-			# ALTERED (S) - I guess this should be moved to be exec. before removing close spikes | moved to line 79
-			# lowrate_spikes_t = lowrate_spikes_t[lowrate_spikes_t > 0]
+		# remove spikes that have negative time steps (not allowed)
+		lowrate_spikes_t = lowrate_spikes_t[lowrate_spikes_t > 0]
 
 		# Updating return values
 		if rate_pre >= rate_post:
@@ -93,4 +120,3 @@ def poisson_spiking_gen(rate_pre, rate_post, t_run, dt, noise):
 			pre_spikes_t = np.sort(lowrate_spikes_t)
 
 	return pre_spikes_t, post_spikes_t
-
