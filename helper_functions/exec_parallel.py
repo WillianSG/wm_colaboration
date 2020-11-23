296754320412911d46fe99cdc50d1bad1cc40e36
Ole Richter <git@olerichter.com>
2020-11-23 13:51:59 +0100

    convert indent to tab

--- helper_functions/exec_parallel.py
+++ helper_functions/exec_parallel.py
@@ -18,51 +18,52 @@ import logging
 # get the python executable
 PYTHON_COMMAND = sys.executable
 if PYTHON_COMMAND is None:
-    PYTHON_COMMAND = "python"
-    logging.warning("python executable path not provided by the system using 'python'")
+	PYTHON_COMMAND = "python"
+	logging.warning("python executable path not provided by the system using 'python'")
 
 # adapt number of workers to system it is running on
 MULTITHREAD_WORKER_THREADS = multiprocessing.cpu_count()
 
 # read from to from command line
 try:
-    EXPERIMENT_FILE = sys.argv[1]
-    MULTITHREAD_ITERATIONS_BEGIN = sys.argv[2]
-    MULTITHREAD_ITERATIONS_END_BEFORE = sys.argv[3]
+	EXPERIMENT_FILE = sys.argv[1]
+	MULTITHREAD_ITERATIONS_BEGIN = sys.argv[2]
+	MULTITHREAD_ITERATIONS_END_BEFORE = sys.argv[3]
 except IndexError:
-    logging.error(f"could not read command line arguments: {sys.argv}")
-    raise SystemExit(f"Usage: {sys.argv[0]} <file to run> <from> <to (excl)>")
+	logging.error(f"could not read command line arguments: {sys.argv}")
+	raise SystemExit(f"Usage: {sys.argv[0]} <file to run> <from> <to (excl)>")
 
 
 def worker():
-    while True:
-        item = q.get()
-        if item is None:
-            break
-        logging.info(f"started: {EXPERIMENT_FILE} seed: {item[0]}")
-        subprocess.call([PYTHON_COMMAND, str(EXPERIMENT_FILE), str(item[0])])
-        logging.info(f"finished: {EXPERIMENT_FILE} seed: {item[0]}")
-        q.task_done()
+	while True:
+		item = q.get()
+		if item is None:
+			break
+		logging.info(f"started: {EXPERIMENT_FILE} seed: {item[0]}")
+		subprocess.call([PYTHON_COMMAND, str(EXPERIMENT_FILE), str(item[0])])
+		logging.info(f"finished: {EXPERIMENT_FILE} seed: {item[0]}")
+		q.task_done()
 
-#create workers
+
+# create workers
 threads = []
 for i in range(MULTITHREAD_WORKER_THREADS):
-    t = threading.Thread(target=worker)
-    t.start()
-    threads.append(t)
+	t = threading.Thread(target=worker)
+	t.start()
+	threads.append(t)
 logging.info(f"{MULTITHREAD_WORKER_THREADS} workers created")
 
 # create task queue
 q = Queue()
 for run_id in range(int(MULTITHREAD_ITERATIONS_BEGIN), int(MULTITHREAD_ITERATIONS_END_BEFORE)):
-    item = [run_id]
-    q.put(item)
+	item = [run_id]
+	q.put(item)
 
 # block until all tasks are done
 q.join()
 
 # stop workers
 for i in range(MULTITHREAD_WORKER_THREADS):
-    q.put(None)
+	q.put(None)
 for t in threads:
-    t.join()
+	t.join()
