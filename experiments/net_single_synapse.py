e84ffc2d4681166e726796ffe5c80c2fd38aed07
WillianSG <w.soares.girao@rug.nl>
2020-12-16 19:07:22 +0100

    single synapse showcase - WIP

--- experiments/net_single_synapse.py
+++ experiments/net_single_synapse.py
@@ -14,6 +14,14 @@ from time import localtime
 import multiprocessing
 prefs.codegen.target = 'numpy'
 
+#=====
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+from matplotlib import gridspec
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.colors as mcolors
+#=====
+
 helper_dir = 'helper_functions'
 
 # get run id as seed for random gens
@@ -53,20 +61,20 @@ from load_neurons import *
 
 # 1 ========== Execution parameters ==========
 
-exp_type = 'showcase'
+exp_type = 'showcase' # 'showcase', 'rates'
 
 # Simulation run variables
 dt_resolution = 0.1/1000 # = 0.0001 sconds (0.1ms) | step of simulation time step resolution
 
-t_run = 3 # simulation time (seconds)
+t_run = 0.3 # simulation time (seconds)
 
 int_meth_syn = 'euler' # Synaptic integration method
 
 # 1.1 ========== Rule's parameters
 
 plasticity_rule = 'LR1' # 'none', 'LR1', 'LR2'
-parameter_set = '1.2' # '2.1'
-bistability = True
+parameter_set = '1.1' # '2.1'
+bistability = False
 
 [tau_xpre,
 	tau_xpost,
@@ -88,7 +96,7 @@ bistability = True
 
 w_init = w_max*rho_init
 
-# 1.2 ==== net parameters
+# 1.2 ========== net parameters
 
 N_Pre = 1
 N_Post = 1
@@ -102,17 +110,15 @@ else:
 	neuron_type = 'poisson'
 
 
-# 2 ========== Learning rule as Brian2's synaptic model
+# 2 ========== Learning rule as Brian2's synaptic model ==========
 [model_E_E,
 	pre_E_E,
 	post_E_E] = load_synapse_model(plasticity_rule, neuron_type, bistability)
 
-
-
 # 3 ========== Brian2's neuron objects
 
 input_pre = np.array([10, 60, 165, 255])/1000
-input_post = np.array([25, 65, 140, 250])/1000 
+input_post = np.array([25, 65, 140, 250])/1000
 
 Pre, Post = load_neurons(
 	N_Pre, N_Post, neuron_type,
@@ -121,6 +127,7 @@ Pre, Post = load_neurons(
 	pre_rate = pre_rate,
 	post_rate =  post_rate)
 
+# 3.1 ========== setting connections between neurons
 Pre_Post = Synapses(
 	source = Pre,
 	target = Post,
@@ -135,6 +142,8 @@ Pre_Post.connect(j = 'i')
 Pre_Post.rho = rho_init
 Pre_Post.w = w_init
 
+# 3.2 ========== Setting simulation monitors
+
 StateMon = StateMonitor(Pre_Post, ['xpre', 'xpost', 'w', 'rho'], record = True)
 
 Pre_spk_mon = SpikeMonitor( 
@@ -147,7 +156,201 @@ Post_spk_mon = SpikeMonitor(
 	record = True,
 	name = 'Post_spk_mon')
 
+# 4. ========== Running network ==========
+
 run(t_run*second)
 
+# 5. ========== Plots ==========
+
+lwdth = 3
+
+s1 = 30
+
+mpl.rcParams['axes.linewidth'] = 1.5
+
+fig = plt.figure(figsize = (15, 22))
+gs = gridspec.GridSpec(10, 1, height_ratios = [2, 2, 1, 4, 1,4, 1 , 4, 1, 4])
+
+# 5.1 ==== Pre neuron spike activity
+
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.vlines(Pre_spk_mon.t/ms, 0.5, 1.75, color = 'blue', linewidth = lwdth)
+
+ax1.axhline(linewidth = lwdth, color = 'blue', y = 0.5)
+ax1.set_yticks([])
+ax1.set_xticklabels([])
+plt.ylabel('Pre ${i}$', fontsize = s1, color = 'black', rotation = 0)
+ax1.yaxis.set_label_coords(-0.15, 0.3)
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+plt.ylim(0, 2)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+
+# 5.2 ==== Post neuron spike activity
+
+ax2 = fig.add_subplot(gs[1, 0])
+ax2.vlines(Post_spk_mon.t/ms, 0.5, 1.75, color = 'red', linewidth = lwdth)
+ax2.axhline(linewidth = lwdth, color = 'red', y = 0.5)
+ax2.set_yticks([])
+ax2.set_xticklabels([])
+
+plt.ylabel('Post ${j}$', fontsize = s1, color = 'black', rotation = 0)
+
+ax2.yaxis.set_label_coords(-0.15, 0.3)
+
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+plt.ylim(0, 2)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+
+# 5.3 ==== Presynaptic calcium trace
+
+ax3 = fig.add_subplot(gs[3, 0])
+ax3.plot(StateMon.t/ms, StateMon.xpre[0], color = 'blue', linewidth = lwdth)
+
+# Adapted learning rule has threshold on pre- trace as well
+if plasticity_rule == 'LR2':
+	ax3.axhline(linestyle = 'dashed', color = 'grey', lw = lwdth/2, 
+		y = thr_pre, 
+		label = '$\\theta_{pre}$')
+
+	plt.legend(loc = 'upper right', prop = {'size':s1-10}, 
+		bbox_to_anchor = (1, 1.3), 
+		ncol = 3)
+
+plt.ylabel('$x_{Pre}$ \n(a.u.)', size = s1, color = 'black',
+	horizontalalignment = 'center', 
+	labelpad = 20)
+
+ax3.yaxis.set_label_coords(-0.15, 0.3)
+ax3.set_xticklabels([])
+
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+
+major_yticks = np.linspace(0, max(StateMon.xpre[0])*1.1, 4)
+
+ax3.set_yticks(np.around(major_yticks, 1))
+
+plt.ylim(0, max(StateMon.xpre[0])*1.1)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+
+# 5.4 ==== Postsynaptic calcium trace
+
+ax4 = fig.add_subplot(gs[5, 0])
+ax4.plot(StateMon.t/ms, StateMon.xpost[0], color = 'red', linewidth = lwdth) 
+ax4.axhline(linestyle = 'dashed', color = 'grey', lw = lwdth/2, y = thr_post,
+	label = '$\\theta_{post}$')
+ax4.set_xticklabels([])
+
+plt.legend(loc = 'upper right', prop = {'size':s1-10}, 
+	bbox_to_anchor = (1, 1.3), 
+	ncol = 3)
+
+plt.ylabel('$x_{Post}$ \n(a.u.) ', size = s1, color = 'black', 
+	horizontalalignment = 'center', 
+	labelpad = 20)
+
+ax4.yaxis.set_label_coords(-0.15, 0.3)
+ax4.set_xticklabels([])
+
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+
+major_yticks = np.linspace(0, max(StateMon.xpost[0])*1.1, 4)
+
+ax4.set_yticks(np.around(major_yticks, 1))
+
+plt.ylim(0, np.around(max(StateMon.xpost[0]), 1)*1.1)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+
+# 5.5 ==== Rho state variable
+
+ax5 = fig.add_subplot(gs[7, 0])
+ax5.axhline(linestyle = 'dashed', color = 'dimgrey', lw = lwdth/2, 
+	y = thr_b_rho, label = '$\\theta_{\\rho}$')
+
+ax5.axhline(linestyle = 'solid', color = 'grey', lw = lwdth/2, 
+	y = rho_max,
+	label = 'UP state')
+ax5.axhline(linestyle = 'solid', color = 'black', lw = lwdth/2, 
+	y = rho_min, 
+	label = 'DOWN state')
+ax5.plot(StateMon.t/ms, StateMon.rho[0], color = 'k', linewidth = lwdth)
+
+plt.legend(loc = 'upper right', prop = {'size':s1-10}, 
+	bbox_to_anchor = (1, 1.3),
+	ncol = 3)
+plt.ylabel('Efficacy $\\rho$ \n(a.u.)', size = s1, 
+	horizontalalignment = 'center', 
+	labelpad = 20)
+
+ax5.set_xticklabels([])
+ax5.yaxis.set_label_coords(-0.15, 0.3)
+
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+
+major_yticks = [rho_min, rho_max/2, rho_max]
+
+ax5.set_yticks(major_yticks)
+
+plt.ylim(-0.25, 1.25)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+
+# 5.6 ==== Weight
+
+ax6 = fig.add_subplot(gs[9, 0])
+ax6.plot(StateMon.t/ms, StateMon.w[0]/mV, label = 'w', color = 'k',
+	linewidth = lwdth) 
+ax6.axhline(color = 'grey', lw = lwdth, y = w_max/mV)
+
+plt.ylabel('Weight $w$\n(mV)', size = s1, horizontalalignment = 'center',
+	labelpad = 20)
+
+ax6.yaxis.set_label_coords(-0.15, 0.3)
+
+plt.tick_params(axis = 'x', which = 'major', width = lwdth, length = 5)
+plt.tick_params(axis = 'y', which = 'major', width = lwdth, length = 0)
+
+major_yticks = [0, w_max/mV/2, w_max/mV]
+
+ax6.set_yticks(major_yticks) 
+
+plt.ylim(-0.25, w_max/mV)
+plt.xlim(0, t_run*1000)
+plt.xticks(size = s1)
+plt.yticks(size = s1)
+plt.xlabel('Time (ms)', size = s1)
+
+# 5.7 ==== Exporting plot to file
+
+results_path = os.path.join(parent_dir, 'plots_results')
+is_dir = os.path.isdir(results_path)
+
+if not(is_dir):
+	os.mkdir(results_path)
+
+if exp_type == 'showcase':
+	plot_name = sim_id + '_' + str(job_seed) + '_' + exp_type + '_' + plasticity_rule + '_' + parameter_set + str(bistability) + '.png'
+else:
+	plot_name = sim_id + '_' + str(job_seed) + '_' + exp_type + '_' + plasticity_rule + '_' + parameter_set + str(bistability) + '_' + str(pre_rate) + '_' + str(post_rate) + '.png'
+
+
+plt.savefig(os.path.join(results_path, plot_name), 
+	bbox_inches = 'tight', 
+	dpi = 200)
+
+# END.
 
 print('\nnet_single_synapse.py - END.\n')
