18f06e2210e23e55f40d5a0cbc6132a45da4e0af
WillianSG <w.soares.girao@rug.nl>
2021-02-23 15:58:26 +0100

    transisiton probabilities

--- synapse_dynamics/stdp_trans_probs.py
+++ synapse_dynamics/stdp_trans_probs.py
@@ -15,6 +15,8 @@ from brian2 import *
 from scipy import *
 from numpy import *
 
+import matplotlib.pyplot as plt
+
 prefs.codegen.target = 'numpy'
 
 helper_dir = 'helper_functions'
@@ -32,6 +34,8 @@ from run_single_synap import *
 
 # == 1 - Simulation run variables ==========
 
+sim_loop_rep = 25
+
 dt_resolution = 0.0001 # 0.1ms | step of simulation time step resolution
 t_run = 0.3 # 300ms | simulation time (seconds)
 noise = 0.75 # introduced noise (diff) between spk times betweem pre- and post-
@@ -42,22 +46,25 @@ N_Post = 1
 plasticity_rule = 'LR2'			# 'none', 'LR1', 'LR2'
 parameter_set = '2.4'			# '2.1', '2.4'
 bistability = False
-w_init = 0.0					# '0.0' to test LTD, '1.0' to test LTP
+w_init = 0.0					# '0.0' to test LTP, '1.0' to test LTD
 
 neuron_type = 'spikegenerator'	# 'poisson', 'LIF' , 'spikegenerator'
 int_meth_syn = 'euler'			# Synaptic integration method
 
-exp_type = 'stdp_trans_probabi'
+exp_type = 'stdp_trans_probabi_' + str(w_init)
 
 # Range of pre- and postsynaptic frequencies (Hz)
 min_freq = 0
-max_freq = 60
+max_freq = 120
+
 step = 5
 
 # == 1.1 - neurons' firing rates
 
 f_pre = np.arange(min_freq, max_freq + 0.1, step)
 f_pos = np.arange(min_freq, max_freq + 0.1, step)
+
+transition_probabilities = []
 
 # == 2 - Brian2 simulation settings ==========
 
@@ -91,43 +98,60 @@ f_pos = np.arange(min_freq, max_freq + 0.1, step)
 # Starts a new scope for magic functions
 start_scope()
 
-result = run_single_synap(
-	pre_rate = f_pre[6], 
-	post_rate = f_pos[2], 
-	t_run = t_run, 
-	dt_resolution = dt_resolution, 
-	plasticity_rule = plasticity_rule, 
-	neuron_type = neuron_type, 
-	noise = noise, 
-	bistability = bistability, 
-	N_Pre = N_Pre, 
-	N_Post = N_Post, 
-	tau_xpre = tau_xpre, 
-	tau_xpost = tau_xpost, 
-	xpre_jump = xpre_jump, 
-	xpost_jump = xpost_jump, 
-	rho_neg = rho_neg, 
-	rho_neg2 = rho_neg2, 
-	rho_init = rho_init, 
-	tau_rho = tau_rho, 
-	thr_post = thr_post, 
-	thr_pre = thr_pre, 
-	thr_b_rho = thr_b_rho, 
-	rho_min = rho_min, 
-	rho_max = rho_max, 
-	alpha = alpha, 
-	beta = beta, 
-	xpre_factor = xpre_factor, 
-	w_max = w_max, 
-	model_E_E = model_E_E, 
-	pre_E_E = pre_E_E, 
-	post_E_E = post_E_E, 
-	int_meth_syn = int_meth_syn)
+for x in range(0, len(f_pos)):
+	for y in range(0, len(f_pre)):
+		result = 0
+		print('> pos: ', f_pos[x], 'Hz | pre: ', f_pre[y], 'Hz')
 
-print('\n final efficacy (w): ', result)
+		for i in range(0, sim_loop_rep):
+			result += run_single_synap(
+				pre_rate = f_pre[y],
+				post_rate = f_pos[x],
+				t_run = t_run,
+				dt_resolution = dt_resolution,
+				plasticity_rule = plasticity_rule,
+				neuron_type = neuron_type,
+				noise = noise,
+				bistability = bistability,
+				N_Pre = N_Pre,
+				N_Post = N_Post,
+				tau_xpre = tau_xpre,
+				tau_xpost = tau_xpost,
+				xpre_jump = xpre_jump,
+				xpost_jump = xpost_jump,
+				rho_neg = rho_neg,
+				rho_neg2 = rho_neg2,
+				rho_init = rho_init,
+				tau_rho = tau_rho,
+				thr_post = thr_post,
+				thr_pre = thr_pre,
+				thr_b_rho = thr_b_rho,
+				rho_min = rho_min,
+				rho_max = rho_max,
+				alpha = alpha,
+				beta = beta,
+				xpre_factor = xpre_factor,
+				w_max = w_max,
+				model_E_E = model_E_E,
+				pre_E_E = pre_E_E,
+				post_E_E = post_E_E,
+				int_meth_syn = int_meth_syn,
+				w_init = w_init)
 
+	transition_probabilities.append(result/sim_loop_rep)
 
-# for f_pos in range(0, len(post_freq)):
-# 	for f_pre in range(0, len(pre_freq)):
-# 		print('post @ ', post_freq[f_pos], 'Hz, pre @ ', pre_freq[f_pre], 'Hz')
-# 	print('\n')
+plt.plot(f_pos, np.array(transition_probabilities))
+
+if w_init == 0.0:
+	plt.title('LTP Transition Probability')
+else:
+	plt.title('LTD Transition Probability')
+
+plt.xlabel('Postsynaptic frequency (Hz)')
+plt.ylabel('Probability')
+
+plt.savefig(exp_type + '_.png')
+
+print('\nstdp_trans_probs.py - END.\n')
+
+		
