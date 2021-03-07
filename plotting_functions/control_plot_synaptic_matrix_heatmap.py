67909cc2d007ff1a58d82e317edc599aed7538d8
WillianSG <w.soares.girao@rug.nl>
2021-03-07 19:13:09 +0100

    plotting heat maps

--- plotting_functions/control_plot_synaptic_matrix_heatmap.py
+++ plotting_functions/control_plot_synaptic_matrix_heatmap.py
@@ -14,42 +14,33 @@ import os, sys, pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-helper_dir = 'helper_functions'
+simulation_data = '20210306_14_15_22_attractor_stability'
 
 # Parent directory
 parent_dir = os.path.dirname(os.getcwd())
 
 # Adding parent dir to list of dirs that the interpreter will search in
-sys.path.append(os.path.join(parent_dir, helper_dir))
+simulation_data_path = os.path.join(
+	parent_dir, 
+	'network_results', 
+	'attractor_stability',
+	simulation_data,
+	'synaptic_matrix_snaps')
 
+pickled_list = sorted(os.listdir(simulation_data_path))
 
-sim_data = 'C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\20210306_13_28_06_synaptic_matrix_after_flat_to_E_fixed_size_offset0_sec1.pickle'
+for x in range(0, len(pickled_list)):
+	img_name = pickled_list[x].split('_')[-1].split('.')[0] + '.png'
 
-with open(sim_data,'rb') as f:(
-	synaptic_matrix) = pickle.load(f)
+	sim_data = os.path.join(simulation_data_path, pickled_list[x])
 
-plt.imshow(synaptic_matrix, cmap='coolwarm')
-plt.colorbar()
-plt.savefig('C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\after_cue.png')
-plt.show()
+	with open(sim_data,'rb') as f:(
+		synaptic_matrix) = pickle.load(f)
 
-sim_data = 'C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\20210306_13_28_06_synaptic_matrix_after_flat_to_E_fixed_size_offset42_sec3.pickle'
+	plt.imshow(synaptic_matrix, cmap='coolwarm')
+	plt.colorbar()
+	plt.savefig(os.path.join(simulation_data_path, img_name))
+	plt.close()
 
-with open(sim_data,'rb') as f:(
-	synaptic_matrix) = pickle.load(f)
-
-plt.imshow(synaptic_matrix, cmap='coolwarm')
-plt.colorbar()
-plt.savefig('C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\after_two.png')
-plt.show()
-
-sim_data = 'C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\20210306_13_28_06_synaptic_matrix_after_flat_to_E_fixed_size_offset84_sec4.pickle'
-
-with open(sim_data,'rb') as f:(
-	synaptic_matrix) = pickle.load(f)
-
-plt.imshow(synaptic_matrix, cmap='coolwarm')
-plt.colorbar()
-plt.savefig('C:\\Users\\willi\\PhD_Stuff\\learning_rule\\network_results\\attractor_stability\\20210306_13_28_06_attractor_stability\\after_all.png')
-plt.show()
+print('\ncontrol_plot_synaptic_matrix_heatmap.py - END.\n')
 
