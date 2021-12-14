8ee5f836a63da845305d3362bfab84f353f667ad
Thomas Tiotto <thomas.tiotto@outlook.com>
2021-12-14 16:47:52 +0100

    - neighbourhood computation

--- tt_tests/network_viz.py
+++ tt_tests/network_viz.py
@@ -9,33 +9,44 @@ from brian2 import second, prefs, NeuronGroup, Synapses
 from plotting_functions.rcn_spiketrains_histograms import plot_rcn_spiketrains_histograms
 
 
-def draw_graph3( networkx_graph, notebook=False, output_filename='graph', open_output=False,
-                 show_buttons=False, only_physics_buttons=False ):
-    """
-    This function accepts a networkx graph object,
-    converts it to a pyvis network object preserving its node and edge attributes,
-    and both returns and saves a dynamic network visualization.
-
-    Valid node attributes include:
-        "size", "value", "title", "x", "y", "label", "color".
-
-        (For more info: https://pyvis.readthedocs.io/en/latest/documentation.html#pyvis.network.Network.add_node)
-
-    Valid edge attributes include:
-        "arrowStrikethrough", "hidden", "physics", "title", "value", "width"
-
-        (For more info: https://pyvis.readthedocs.io/en/latest/documentation.html#pyvis.network.Network.add_edge)
-
-
-    Args:
-        networkx_graph: The graph to convert and display
-        notebook: Display in Jupyter?
-        output_filename: Where to save the converted network
-        show_buttons: Show buttons in saved version of network?
-        only_physics_buttons: Show only buttons controlling physics of network?
-    """
+def tag_weakly_connected_components( g ):
+    import networkx as nx
     
-    # import
+    idx_components = { u: i for i, node_set in enumerate( nx.weakly_connected_components( g ) ) for u in node_set }
+    for n, i in idx_components.items():
+        g.nodes[ n ][ 'attractor' ] = i
+
+
+def tag_attracting_components( g ):
+    import networkx as nx
+    from networkx.algorithms.components import is_attracting_component
+    
+    attracting_map = { }
+    for atr in set( nx.get_node_attributes( g, 'attractor' ).values() ):
+        atr_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'attractor' ] == atr ]
+        atr_subgraph = g.subgraph( atr_nodes )
+        is_attracting = is_attracting_component( atr_subgraph )
+        for n in atr_nodes:
+            attracting_map[ n ] = { 'is_attracting': is_attracting }
+    
+    nx.set_node_attributes( g, attracting_map )
+
+
+def colour_by_attractor( g ):
+    import networkx as nx
+    import cmasher as cmr
+    
+    num_attractors = len( set( nx.get_node_attributes( g, 'attractor' ).values() ) )
+    colours = cmr.take_cmap_colors( 'tab20', num_attractors, return_fmt='hex' )
+    
+    for n, v in g.nodes( data=True ):
+        g.nodes[ n ][ 'color' ] = colours[ g.nodes[ n ][ 'attractor' ] ]
+
+
+def draw_graph3( networkx_graph, notebook=False, output_filename='graph',
+                 scale_by='',
+                 open_output=False,
+                 show_buttons=False, only_physics_buttons=False ):
     from pyvis import network as net
     
     # make a pyvis network
@@ -44,7 +55,6 @@ def draw_graph3( networkx_graph, notebook=False, output_filename='graph', open_o
     # for each node and its attributes in the networkx graph
     for node, node_attrs in networkx_graph.nodes( data=True ):
         pyvis_graph.add_node( node, **node_attrs )
-    #         print(node,node_attrs)
     
     # for each edge and its attributes in the networkx graph
     for source, target, edge_attrs in networkx_graph.edges( data=True ):
@@ -58,9 +68,15 @@ def draw_graph3( networkx_graph, notebook=False, output_filename='graph', open_o
     # add neighbor data to node hover data
     neighbour_map = pyvis_graph.get_adj_list()
     for node in pyvis_graph.nodes:
-        node[ 'title' ] += ' Neighbors:<br>' + '<br>'.join( neighbour_map[ node[ 'id' ] ] )
-        # TODO scale node value by activity
-        # node[ 'value' ] = len( neighbour_map[ node[ 'id' ] ] )
+        node[ 'title' ] += f' Node: {node[ "id" ]}<br>' + ' Excites:<br>' + '<br>'.join(
+                node[ 'excites' ] ) + '<br> Inhibited by:<br>' + '<br>'.join( node[ 'inhibitors' ] )
+        # TODO scale node value by activity or other metrics
+        if scale_by == 'neighbours':
+            node[ 'value' ] = len( neighbour_map[ node[ 'id' ] ] )
+        elif scale_by == 'activity':
+            pass
+        elif scale_by == 'inhibition':
+            pass
     for edge in pyvis_graph.edges:
         edge[ 'title' ] = ' Weight:<br>' + str( edge[ 'weight' ] )
     
@@ -86,10 +102,11 @@ def draw_graph3( networkx_graph, notebook=False, output_filename='graph', open_o
 
 
 # Build NetworkX graph
-def rcn2nx( net, e_neurons, i_neurons, remove_zero_weight_edges=True, output_filename='graph' ):
+def rcn2nx( net, e_neurons, i_neurons, remove_zero_weight_edges=True, output_filename='graph',
+            colour_attractors=True ):
     import networkx as nx
     
-    # ------- workaround for bug in Brian2
+    # ------- workaround for bug in Brian2 (not needed?)
     # import brian2._version
     # from packaging import version
     #
@@ -102,7 +119,6 @@ def rcn2nx( net, e_neurons, i_neurons, remove_zero_weight_edges=True, output_fil
     e2e_edges_pre = net.E_E.i[ e_neurons, e_neurons ].tolist()
     e2e_edges_post = net.E_E.j[ e_neurons, e_neurons ].tolist()
     e2e_edge_weights = net.E_E.w_[ e_neurons, e_neurons ].tolist()
-    
     i2e_edges_pre = net.I_E.i[ i_neurons, e_neurons ].tolist()
     i2e_edges_post = net.I_E.j[ i_neurons, e_neurons ].tolist()
     i2e_edge_weights = net.I_E.w_[ i_neurons, e_neurons ].tolist()
@@ -114,15 +130,34 @@ def rcn2nx( net, e_neurons, i_neurons, remove_zero_weight_edges=True, output_fil
     
     g = nx.DiGraph()
     
-    g.add_nodes_from( [ f'e_{i}' for i in e_neurons ], color='blue', title='', type='excitatory' )
+    # Add excitatory neurons
+    e_nodes = [ f'e_{i}' for i in e_neurons ]
+    g.add_nodes_from( e_nodes, color='blue', title='', type='excitatory' )
     for i, j, w in zip( e2e_edges_pre, e2e_edges_post, e2e_edge_weights ):
         g.add_edge( f'e_{i}', f'e_{j}', weight=w )
-    g.add_nodes_from( [ f'i_{i}' for i in i_neurons ], color='red', title='', type='inhibitory' )
+    
+    # Classify excitatory neurons
+    tag_weakly_connected_components( g )
+    tag_attracting_components( g )
+    if colour_attractors:
+        colour_by_attractor( g )
+    
+    # Add inhibitory neurons
+    i_nodes = [ f'i_{i}' for i in i_neurons ]
+    g.add_nodes_from( i_nodes, color='red', title='', type='inhibitory' )
     for i, j, w in zip( i2e_edges_pre, i2e_edges_post, i2e_edge_weights ):
         g.add_edge( f'i_{i}', f'e_{j}', weight=w )
     
-    if output_filename:
-        nx.write_graphml( g, f'{os.getcwd()}/{output_filename}.graphml' )
+    # compute neighbourhoods
+    for n in e_nodes:
+        g.nodes[ n ][ 'inhibitors' ] = [ i for i in list( g.predecessors( n ) ) if 'i_' in i ]
+        g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
+    for n in i_nodes:
+        g.nodes[ n ][ 'inhibitors' ] = [ ]
+        g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
+    
+    # if output_filename:
+    #     nx.write_graphml( g, f'{os.getcwd()}/{output_filename}.graphml' )
     
     return g
 
@@ -214,33 +249,12 @@ plot_rcn_spiketrains_histograms(
         show=True )
 
 import networkx as nx
-
-
-def tag_weakly_connected_components( g ):
-    idx_components = { u: i for i, node_set in enumerate( nx.weakly_connected_components( g ) ) for u in node_set }
-    for n, i in idx_components.items():
-        g.nodes[ n ][ 'attractor' ] = i
-
-
-def colour_by_attractor( g ):
-    import cmasher as cmr
-    
-    num_attractors = len( set( nx.get_node_attributes( g, 'attractor' ).values() ) )
-    colors = cmr.take_cmap_colors( 'viridis', num_attractors, return_fmt='hex' )
-    
-    for n, v in g.nodes( data=True ):
-        g.nodes[ n ][ 'color' ] = colors[ g.nodes[ n ][ 'attractor' ] ]
-
-
-e_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'type' ] == 'excitatory' ]
-e_subgraph = g.subgraph( e_nodes )
-
-tag_weakly_connected_components( e_subgraph )
-colour_by_attractor( e_subgraph )
-
-from networkx.algorithms import community
-
-comp = community.girvan_newman( e_subgraph )
-print( tuple( sorted( c ) for c in next( comp ) ) )
-
-draw_graph3( e_subgraph, output_filename='third', open_output=True, show_buttons=True )
+#
+# e_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'type' ] == 'excitatory' ]
+# e_subgraph = g.subgraph( e_nodes )
+#
+# tag_weakly_connected_components( e_subgraph )
+# tag_attracting_components( e_subgraph )
+# colour_by_attractor( e_subgraph )
+#
+# draw_graph3( e_subgraph, output_filename='third', open_output=True, show_buttons=True )
