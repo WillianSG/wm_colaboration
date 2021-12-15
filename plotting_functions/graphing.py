49ddade66d1627f883c48151ef08b6d3a575af6f
Thomas Tiotto <thomas.tiotto@outlook.com>
2021-12-15 18:55:20 +0100

    - added attractor_inhibition function to quantify the amount of inhibition each attractor is receiving

--- plotting_functions/graphing.py
+++ plotting_functions/graphing.py
@@ -3,45 +3,45 @@ import numpy as np
 
 
 def tag_weakly_connected_components( g ):
-    idx_components = { n: i for i, node_set in enumerate( nx.weakly_connected_components( g ) ) for n in node_set if
-                       'e_' in n }
-    for n, i in idx_components.items():
-        g.nodes[ n ][ 'attractor' ] = i
+	idx_components = { n: i for i, node_set in enumerate( nx.weakly_connected_components( g ) ) for n in node_set if
+	                   'e_' in n }
+	for n, i in idx_components.items():
+		g.nodes[ n ][ 'attractor' ] = i
 
 
 def tag_attracting_components( g ):
-    from networkx.algorithms.components import is_attracting_component
-    
-    attracting_map = { }
-    for atr in set( nx.get_node_attributes( g, 'attractor' ).values() ):
-        atr_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'attractor' ] == atr ]
-        atr_subgraph = g.subgraph( atr_nodes )
-        is_attracting = is_attracting_component( atr_subgraph )
-        for n in atr_nodes:
-            attracting_map[ n ] = { 'is_attracting': is_attracting }
-    
-    nx.set_node_attributes( g, attracting_map )
+	from networkx.algorithms.components import is_attracting_component
+	
+	attracting_map = { }
+	for atr in set( nx.get_node_attributes( g, 'attractor' ).values() ):
+		atr_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'attractor' ] == atr ]
+		atr_subgraph = g.subgraph( atr_nodes )
+		is_attracting = is_attracting_component( atr_subgraph )
+		for n in atr_nodes:
+			attracting_map[ n ] = { 'is_attracting': is_attracting }
+	
+	nx.set_node_attributes( g, attracting_map )
 
 
 def rgb_to_hex( r, g, b ):
-    return ('#{:02X}{:02X}{:02X}').format( r, g, b )
+	return ('#{:02X}{:02X}{:02X}').format( r, g, b )
 
 
 def hex_to_rgb( h ):
-    h = h.lstrip( '#' )
-    
-    return tuple( int( h[ i:i + 2 ], 16 ) for i in (0, 2, 4) )
+	h = h.lstrip( '#' )
+	
+	return tuple( int( h[ i:i + 2 ], 16 ) for i in (0, 2, 4) )
 
 
 def colour_by_attractor( g ):
-    import cmasher as cmr
-    
-    num_attractors = len( set( nx.get_node_attributes( g, 'attractor' ).values() ) )
-    colours = cmr.take_cmap_colors( 'tab20', num_attractors, return_fmt='int' )
-    
-    for n, v in g.nodes( data=True ):
-        col = list( colours[ g.nodes[ n ][ 'attractor' ] ] )
-        g.nodes[ n ][ 'color' ] = f'rgba({col[ 0 ]},{col[ 1 ]},{col[ 2 ]},1)'
+	import cmasher as cmr
+	
+	num_attractors = len( set( nx.get_node_attributes( g, 'attractor' ).values() ) )
+	colours = cmr.take_cmap_colors( 'tab20', num_attractors, return_fmt='int' )
+	
+	for n, v in g.nodes( data=True ):
+		col = list( colours[ g.nodes[ n ][ 'attractor' ] ] )
+		g.nodes[ n ][ 'color' ] = f'rgba({col[ 0 ]},{col[ 1 ]},{col[ 2 ]},1)'
 
 
 # TODO remove edges smaller than max_weights?
@@ -50,87 +50,88 @@ def colour_by_attractor( g ):
 def rcn2nx( net, neurons_subsample=None, subsample_attractors=False, seed=None,
             remove_zero_weight_edges=True, output_filename='graph',
             colour_attractors=True ):
-    import os
-    
-    # ------ Subsample graph nodes
-    if neurons_subsample:
-        e_neurons_subsample = np.clip( neurons_subsample[ 0 ], 0, len( net.E ) )
-        i_neurons_subsample = np.clip( neurons_subsample[ 1 ], 0, len( net.I ) )
-        
-        if seed:
-            np.random.seed( seed )
-        if subsample_attractors:
-            assert e_neurons_subsample % 3 == 0
-            e_neurons = np.concatenate(
-                    [ np.random.choice( range( 0, 64 ), int( e_neurons_subsample / 3 ), replace=False ),
-                      np.random.choice( range( 100, 164 ), int( e_neurons_subsample / 3 ), replace=False ),
-                      np.random.choice( range( 164, 256 ), int( e_neurons_subsample / 3 ), replace=False ) ]
-                    )
-            i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
-        else:
-            e_neurons = np.random.choice( range( 0, len( net.E ) ), e_neurons_subsample, replace=False )
-            i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
-    else:
-        e_neurons = np.array( range( 0, len( net.E ) ) )
-        i_neurons = np.array( range( 0, len( net.I ) ) )
-    
-    # ------- workaround for bug in Brian2 (not needed?)
-    # import brian2._version
-    # from packaging import version
-    #
-    # brian_version = brian2._version.get_versions()[ 'version' ]
-    # if version.parse( brian_version ) > version.parse( '2.5' ):
-    #     syn_indices = np.arange( len( net.I_E ) )[ np.isin( net.I_E.i, i_neurons ) & np.isin( net.I_E.j, e_neurons ) ]
-    #
-    #     syn_indices_2 = [ (i, j) for i, j in zip( net.I_E.i, net.I_E.j ) if i in i_neurons and j in e_neurons ]
-    # else:
-    e2e_edges_pre = net.E_E.i[ e_neurons, e_neurons ].tolist()
-    e2e_edges_post = net.E_E.j[ e_neurons, e_neurons ].tolist()
-    e2e_edge_weights = net.E_E.w_[ e_neurons, e_neurons ].tolist()
-    i2e_edges_pre = net.I_E.i[ i_neurons, e_neurons ].tolist()
-    i2e_edges_post = net.I_E.j[ i_neurons, e_neurons ].tolist()
-    i2e_edge_weights = net.I_E.w_[ i_neurons, e_neurons ].tolist()
-    
-    if remove_zero_weight_edges:
-        e2e_edges_pre = [ k for i, k in enumerate( e2e_edges_pre ) if e2e_edge_weights[ i ] != 0 ]
-        e2e_edges_post = [ k for i, k in enumerate( e2e_edges_post ) if e2e_edge_weights[ i ] != 0 ]
-        e2e_edge_weights = [ k for i, k in enumerate( e2e_edge_weights ) if e2e_edge_weights[ i ] != 0 ]
-    
-    g = nx.DiGraph()
-    
-    # Add excitatory neurons
-    e_nodes = [ f'e_{i}' for i in e_neurons ]
-    for n in e_nodes:
-        g.add_node( n, label=n, color='rgba(0,0,255,1)', title='', type='excitatory' )
-    for i, j, w in zip( e2e_edges_pre, e2e_edges_post, e2e_edge_weights ):
-        g.add_edge( f'e_{i}', f'e_{j}', weight=w )
-    
-    # Classify excitatory neurons
-    tag_weakly_connected_components( g )
-    tag_attracting_components( g )
-    if colour_attractors:
-        colour_by_attractor( g )
-    
-    # Add inhibitory neurons
-    i_nodes = [ f'i_{i}' for i in i_neurons ]
-    for n in i_nodes:
-        g.add_node( n, label=n, color='rgba(255,0,0,0.5)', title='', type='inhibitory' )
-    for i, j, w in zip( i2e_edges_pre, i2e_edges_post, i2e_edge_weights ):
-        g.add_edge( f'i_{i}', f'e_{j}', weight=w )
-    
-    #  GraphML does not support list or any non-primitive type, so we save before computing them
-    if output_filename:
-        nx.write_graphml( g, f'{os.getcwd()}/{output_filename}.graphml' )
-    
-    # compute neighbourhoods
-    # for n in e_nodes:
-    #     g.nodes[ n ][ 'inhibitors' ] = [ i for i in list( g.predecessors( n ) ) if 'i_' in i ]
-    #     g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
-    # for n in i_nodes:
-    #     g.nodes[ n ][ 'inhibitors' ] = [ ]
-    #     g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
-    
-    return g
+	import os
+	
+	# ------ Subsample graph nodes
+	if neurons_subsample:
+		e_neurons_subsample = np.clip( neurons_subsample[ 0 ], 0, len( net.E ) )
+		i_neurons_subsample = np.clip( neurons_subsample[ 1 ], 0, len( net.I ) )
+		
+		if seed:
+			np.random.seed( seed )
+		if subsample_attractors:
+			assert e_neurons_subsample % 3 == 0
+			e_neurons = np.concatenate(
+					[ np.random.choice( range( 0, 64 ), int( e_neurons_subsample / 3 ), replace=False ),
+					  np.random.choice( range( 100, 164 ), int( e_neurons_subsample / 3 ), replace=False ),
+					  np.random.choice( range( 164, 256 ), int( e_neurons_subsample / 3 ), replace=False ) ]
+					)
+			i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
+		else:
+			e_neurons = np.random.choice( range( 0, len( net.E ) ), e_neurons_subsample, replace=False )
+			i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
+	else:
+		e_neurons = np.array( range( 0, len( net.E ) ) )
+		i_neurons = np.array( range( 0, len( net.I ) ) )
+	
+	# ------- workaround for bug in Brian2 (not needed?)
+	# import brian2._version
+	# from packaging import version
+	#
+	# brian_version = brian2._version.get_versions()[ 'version' ]
+	# if version.parse( brian_version ) > version.parse( '2.5' ):
+	#     syn_indices = np.arange( len( net.I_E ) )[ np.isin( net.I_E.i, i_neurons ) & np.isin( net.I_E.j,
+	#     e_neurons ) ]
+	#
+	#     syn_indices_2 = [ (i, j) for i, j in zip( net.I_E.i, net.I_E.j ) if i in i_neurons and j in e_neurons ]
+	# else:
+	e2e_edges_pre = net.E_E.i[ e_neurons, e_neurons ].tolist()
+	e2e_edges_post = net.E_E.j[ e_neurons, e_neurons ].tolist()
+	e2e_edge_weights = net.E_E.w_[ e_neurons, e_neurons ].tolist()
+	i2e_edges_pre = net.I_E.i[ i_neurons, e_neurons ].tolist()
+	i2e_edges_post = net.I_E.j[ i_neurons, e_neurons ].tolist()
+	i2e_edge_weights = net.I_E.w_[ i_neurons, e_neurons ].tolist()
+	
+	if remove_zero_weight_edges:
+		e2e_edges_pre = [ k for i, k in enumerate( e2e_edges_pre ) if e2e_edge_weights[ i ] != 0 ]
+		e2e_edges_post = [ k for i, k in enumerate( e2e_edges_post ) if e2e_edge_weights[ i ] != 0 ]
+		e2e_edge_weights = [ k for i, k in enumerate( e2e_edge_weights ) if e2e_edge_weights[ i ] != 0 ]
+	
+	g = nx.DiGraph()
+	
+	# Add excitatory neurons
+	e_nodes = [ f'e_{i}' for i in e_neurons ]
+	for n in e_nodes:
+		g.add_node( n, label=n, color='rgba(0,0,255,1)', title='', type='excitatory' )
+	for i, j, w in zip( e2e_edges_pre, e2e_edges_post, e2e_edge_weights ):
+		g.add_edge( f'e_{i}', f'e_{j}', weight=w )
+	
+	# Classify excitatory neurons
+	tag_weakly_connected_components( g )
+	tag_attracting_components( g )
+	if colour_attractors:
+		colour_by_attractor( g )
+	
+	# Add inhibitory neurons
+	i_nodes = [ f'i_{i}' for i in i_neurons ]
+	for n in i_nodes:
+		g.add_node( n, label=n, color='rgba(255,0,0,0.5)', title='', type='inhibitory' )
+	for i, j, w in zip( i2e_edges_pre, i2e_edges_post, i2e_edge_weights ):
+		g.add_edge( f'i_{i}', f'e_{j}', weight=w )
+	
+	#  GraphML does not support list or any non-primitive type, so we save before computing them
+	if output_filename:
+		nx.write_graphml( g, f'{os.getcwd()}/{output_filename}.graphml' )
+	
+	# compute neighbourhoods
+	# for n in e_nodes:
+	#     g.nodes[ n ][ 'inhibitors' ] = [ i for i in list( g.predecessors( n ) ) if 'i_' in i ]
+	#     g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
+	# for n in i_nodes:
+	#     g.nodes[ n ][ 'inhibitors' ] = [ ]
+	#     g.nodes[ n ][ 'excites' ] = list( g.successors( n ) )
+	
+	return g
 
 
 # TODO fade non-neighbourhood on click
@@ -139,77 +140,111 @@ def nx2pyvis( networkx_graph, notebook=False, output_filename='graph',
               scale_by='',
               open_output=False,
               show_buttons=False, only_physics_buttons=False ):
-    from pyvis import network as net
-    
-    # make a pyvis network
-    pyvis_graph = net.Network( notebook=notebook )
-    pyvis_graph.width = '1000px'
-    pyvis_graph.height = '1000px'
-    # for each node and its attributes in the networkx graph
-    for node, node_attrs in networkx_graph.nodes( data=True ):
-        pyvis_graph.add_node( node, **node_attrs )
-    
-    # for each edge and its attributes in the networkx graph
-    for source, target, edge_attrs in networkx_graph.edges( data=True ):
-        # if value/width not specified directly, and weight is specified, set 'value' to 'weight'
-        if not 'value' in edge_attrs and not 'width' in edge_attrs and 'weight' in edge_attrs:
-            # place at key 'value' the weight of the edge
-            if 'i_' not in source and 'i_' not in target:
-                edge_attrs[ 'value' ] = edge_attrs[ 'weight' ]
-            else:
-                edge_attrs[ 'value' ] = ''
-        # add the edge
-        pyvis_graph.add_edge( source, target, **edge_attrs, title='', arrows='to', dashes=True )
-    
-    # add neighbor data to node hover data
-    neighbour_map = pyvis_graph.get_adj_list()
-    type_colours = { 'excitatory': 'blue', 'inhibitory': 'red' }
-    for node in pyvis_graph.nodes:
-        node[ 'title' ] += f'<center><h2>{node[ "id" ]}</h2></center>'
-        node[ 'title' ] += f'<h3 style="color: {type_colours[ node[ "type" ] ]}">Kind: {node[ "type" ]}</h3>'
-        if 'e_' in node[ 'id' ]:
-            node[ 'title' ] += f'<h3 style="color: {node[ "color" ]}">Attractor: {node[ "attractor" ]}</h3>'
-        node[ 'title' ] += '<h4>Excites:</h4>' + ' '.join(
-                [ f'<p style="color: {networkx_graph.nodes[ n ][ "color" ]}">{n} ('
-                  f'{networkx_graph.nodes[ n ][ "attractor" ]})</p>'
-                  for n in neighbour_map[ node[ 'id' ] ] if 'e_' in n ]
-                )
-        if 'e_' in node[ 'id' ]:
-            node[ 'title' ] += '<h4>Inhibited by:</h4>' + ' '.join(
-                    [ f'<p style="color: red">{n}</p>' for n in neighbour_map[ node[ 'id' ] ] if 'i_' in n ]
-                    )
-        
-        # TODO scale node value by activity or other metrics
-        if scale_by == 'neighbours':
-            node[ 'value' ] = len( neighbour_map[ node[ 'id' ] ] )
-        elif scale_by == 'activity':
-            pass
-        elif scale_by == 'inhibition':
-            pass
-    
-    for edge in pyvis_graph.edges:
-        edge[
-            'title' ] += f'<center><h3 style="color: ' \
-                         f'{type_colours[ networkx_graph.nodes[ edge[ "from" ] ][ "type" ] ]}">{edge[ "from" ]} → ' \
-                         f'{edge[ "to" ]}</h3></center>'
-        edge[ 'title' ] += 'Weight: ' + str( edge[ 'weight' ] )
-    
-    # turn buttons on
-    if show_buttons:
-        if only_physics_buttons:
-            pyvis_graph.show_buttons( filter_=[ 'physics' ] )
-        else:
-            pyvis_graph.show_buttons()
-    
-    pyvis_graph.set_edge_smooth( 'dynamic' )
-    pyvis_graph.toggle_hide_edges_on_drag( True )
-    pyvis_graph.force_atlas_2based()
-    
-    # return and also save
-    pyvis_graph.show( f'{output_filename}.html' )
-    
-    if open_output:
-        import webbrowser
-        import os
-        
-        webbrowser.open( f'file://{os.getcwd()}/{output_filename}.html' )
+	from pyvis import network as net
+	
+	# make a pyvis network
+	pyvis_graph = net.Network( notebook=notebook )
+	pyvis_graph.width = '1000px'
+	pyvis_graph.height = '1000px'
+	# for each node and its attributes in the networkx graph
+	for node, node_attrs in networkx_graph.nodes( data=True ):
+		pyvis_graph.add_node( node, **node_attrs )
+	
+	# for each edge and its attributes in the networkx graph
+	for source, target, edge_attrs in networkx_graph.edges( data=True ):
+		# if value/width not specified directly, and weight is specified, set 'value' to 'weight'
+		if not 'value' in edge_attrs and not 'width' in edge_attrs and 'weight' in edge_attrs:
+			# place at key 'value' the weight of the edge
+			if 'i_' not in source and 'i_' not in target:
+				edge_attrs[ 'value' ] = edge_attrs[ 'weight' ]
+			else:
+				edge_attrs[ 'value' ] = ''
+		# add the edge
+		pyvis_graph.add_edge( source, target, **edge_attrs, title='', arrows='to', dashes=True )
+	
+	# add neighbor data to node hover data
+	neighbour_map = pyvis_graph.get_adj_list()
+	type_colours = { 'excitatory': 'blue', 'inhibitory': 'red' }
+	for node in pyvis_graph.nodes:
+		node[ 'title' ] += f'<center><h2>{node[ "id" ]}</h2></center>'
+		node[ 'title' ] += f'<h3 style="color: {type_colours[ node[ "type" ] ]}">Kind: {node[ "type" ]}</h3>'
+		if 'e_' in node[ 'id' ]:
+			node[ 'title' ] += f'<h3 style="color: {node[ "color" ]}">Attractor: {node[ "attractor" ]}</h3>'
+		node[ 'title' ] += '<h4>Excites:</h4>' + ' '.join(
+				[ f'<p style="color: {networkx_graph.nodes[ n ][ "color" ]}">{n} ('
+				  f'{networkx_graph.nodes[ n ][ "attractor" ]})</p>'
+				  for n in neighbour_map[ node[ 'id' ] ] if 'e_' in n ]
+				)
+		if 'e_' in node[ 'id' ]:
+			node[ 'title' ] += '<h4>Inhibited by:</h4>' + ' '.join(
+					[ f'<p style="color: red">{n}</p>' for n in neighbour_map[ node[ 'id' ] ] if 'i_' in n ]
+					)
+		
+		# TODO scale node value by activity or other metrics
+		if scale_by == 'neighbours':
+			node[ 'value' ] = len( neighbour_map[ node[ 'id' ] ] )
+		elif scale_by == 'activity':
+			pass
+		elif scale_by == 'inhibition':
+			pass
+	
+	for edge in pyvis_graph.edges:
+		edge[ 'title' ] += f'<center><h3 style="color: ' \
+		                   f'{type_colours[ networkx_graph.nodes[ edge[ "from" ] ][ "type" ] ]}">{edge[ "from" ]} → ' \
+		                   f'{edge[ "to" ]}</h3></center>'
+		edge[ 'title' ] += f'<p style="color: {type_colours[ networkx_graph.nodes[ edge[ "from" ] ][ "type" ] ]}">' \
+		                   f'Kind: {networkx_graph.nodes[ edge[ "from" ] ][ "type" ]}</p>'
+		edge[ 'title' ] += 'Weight: ' + str( edge[ 'weight' ] )
+	
+	# turn buttons on
+	if show_buttons:
+		if only_physics_buttons:
+			pyvis_graph.show_buttons( filter_=[ 'physics' ] )
+		else:
+			pyvis_graph.show_buttons()
+	
+	pyvis_graph.set_edge_smooth( 'dynamic' )
+	pyvis_graph.toggle_hide_edges_on_drag( True )
+	pyvis_graph.force_atlas_2based( damping=0.7 )
+	
+	# return and also save
+	pyvis_graph.show( f'{output_filename}.html' )
+	
+	if open_output:
+		import webbrowser
+		import os
+		
+		webbrowser.open( f'file://{os.getcwd()}/{output_filename}.html' )
+
+
+# TODO how much inhibition is each attractor giving?
+# TODO how many inhibitory neurons are shared between attractors?
+# TODO measure net exhitation between attractors
+"""Compute what fraction of inhibitory cells connected to each attractor and the number of these connections to
+quantify the degree of inhibition of each attractor.
+For ex. attractor_inhibition_amount={0: (0.8, 5), 1: (0.8, 4)} means that attractor 0 is receiving input from 80% of
+inhibitory cells via 5 connections, and attractor 1 from 80% via 4.
+"""
+
+
+def attractor_inhibition( input ):
+	from networkx.algorithms.centrality import group_in_degree_centrality
+	
+	if isinstance( input, nx.Graph ):
+		g = input
+	else:
+		g = rcn2nx( input )
+	
+	attractor_inhibition_amount = { }
+	
+	for i in set( nx.get_node_attributes( g, 'attractor' ).values() ):
+		attractor_nodes = [ n for n, v in g.nodes( data=True ) if 'e_' in n and v[ 'attractor' ] == i ]
+		inhibitory_nodes = [ n for n in g.nodes if 'i_' in n ]
+		subgraph = g.subgraph( attractor_nodes + inhibitory_nodes )
+		
+		attractor_inhibition_amount[ i ] = (
+				group_in_degree_centrality( subgraph, attractor_nodes ),
+				len( [ e for e in g.edges if e[ 0 ] in inhibitory_nodes and e[ 1 ] in attractor_nodes ] )
+				)
+	
+	return attractor_inhibition_amount
--- tt_tests/network_viz.py
+++ tt_tests/network_viz.py
@@ -24,10 +24,10 @@ parameter_set = '2.0'
 # parameter_set = '2.0'
 
 rcn = RecurrentCompetitiveNet(
-        plasticity_rule=plasticity_rule,
-        parameter_set=parameter_set,
-        t_run=3 * second,
-        save_path=os.getcwd() )
+		plasticity_rule=plasticity_rule,
+		parameter_set=parameter_set,
+		t_run=3 * second,
+		save_path=os.getcwd() )
 
 rcn.stimulus_pulse = True
 
@@ -36,8 +36,8 @@ rcn.net_init()
 # select neurons from both attractors
 
 
-g = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='initial' )
-nx2pyvis( g, output_filename='initial', open_output=True, show_buttons=True, only_physics_buttons=True )
+g_initial = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='initial' )
+nx2pyvis( g_initial, output_filename='initial', open_output=True, show_buttons=True, only_physics_buttons=True )
 
 # -------- First attractor
 
@@ -48,8 +48,10 @@ rcn.set_stimulus_i( stimulus='flat_to_I', frequency=rcn.stim_freq_i )
 
 rcn.run_net( period=2 )
 
-g = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='first' )
-nx2pyvis( g, output_filename='first', open_output=True, show_buttons=True, only_physics_buttons=True )
+g_first = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='first' )
+nx2pyvis( g_first, output_filename='first', open_output=True, show_buttons=True, only_physics_buttons=True )
+
+print( attractor_inhibition( g_first ) )
 
 # --------- Second attractor
 
@@ -61,28 +63,32 @@ rcn.set_stimulus_i( stimulus='flat_to_I', frequency=rcn.stim_freq_i )
 
 rcn.run_net( period=2 )
 
-g = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='second' )
-nx2pyvis( g, output_filename='second', open_output=True, show_buttons=True, only_physics_buttons=True )
+g_second = rcn2nx( rcn, neurons_subsample=(15, 5), subsample_attractors=True, output_filename='second' )
+nx2pyvis( g_second, output_filename='second', open_output=True, show_buttons=True, only_physics_buttons=True )
+
+print( attractor_inhibition( g_second ) )
+print( attractor_inhibition( rcn ) )
 
 # TODO save plots and graphs in same RCN directory
 plot_rcn_spiketrains_histograms(
-        Einp_spks=rcn.get_Einp_spks()[ 0 ],
-        Einp_ids=rcn.get_Einp_spks()[ 1 ],
-        stim_E_size=rcn.stim_size_e,
-        E_pop_size=rcn.N_input_e,
-        Iinp_spks=rcn.get_Iinp_spks()[ 0 ],
-        Iinp_ids=rcn.get_Iinp_spks()[ 1 ],
-        stim_I_size=rcn.stim_size_i,
-        I_pop_size=rcn.N_input_i,
-        E_spks=rcn.get_E_spks()[ 0 ],
-        E_ids=rcn.get_E_spks()[ 1 ],
-        I_spks=rcn.get_I_spks()[ 0 ],
-        I_ids=rcn.get_I_spks()[ 1 ],
-        t_run=6,
-        path_to_plot=os.getcwd(),
-        show=True )
+		Einp_spks=rcn.get_Einp_spks()[ 0 ],
+		Einp_ids=rcn.get_Einp_spks()[ 1 ],
+		stim_E_size=rcn.stim_size_e,
+		E_pop_size=rcn.N_input_e,
+		Iinp_spks=rcn.get_Iinp_spks()[ 0 ],
+		Iinp_ids=rcn.get_Iinp_spks()[ 1 ],
+		stim_I_size=rcn.stim_size_i,
+		I_pop_size=rcn.N_input_i,
+		E_spks=rcn.get_E_spks()[ 0 ],
+		E_ids=rcn.get_E_spks()[ 1 ],
+		I_spks=rcn.get_I_spks()[ 0 ],
+		I_ids=rcn.get_I_spks()[ 1 ],
+		t_run=6,
+		path_to_plot=os.getcwd(),
+		show=True )
 
 import networkx as nx
+
 #
 # e_nodes = [ n for n, v in g.nodes( data=True ) if v[ 'type' ] == 'excitatory' ]
 # e_subgraph = g.subgraph( e_nodes )
