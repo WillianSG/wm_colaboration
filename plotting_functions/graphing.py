b62c590456e3998afed410c19a4032b979d98c24
Thomas Tiotto <thomas.tiotto@outlook.com>
2021-12-17 12:48:02 +0100

    - added normalise to attractor_inhibition - added docstrings to functions in graphing.py

--- plotting_functions/graphing.py
+++ plotting_functions/graphing.py
@@ -12,40 +12,53 @@ def hex_to_rgb( h ):
 	return tuple( int( h[ i:i + 2 ], 16 ) for i in (0, 2, 4) )
 
 
-def rcn2nx( net, neurons_subsample=None, subsample_attractors=False, seed=None,
-            remove_edges_threshold=0, output_filename='graph' ):
+def rcn2nx( rcn, neurons_subsample=None, subsample_attractors=False, seed=None,
+            remove_edges_threshold=0.0, output_filename='graph' ):
+	"""Given an instance of a RecurrentCompetitiveNet builds the corresponding NetworkX graph.
+	
+	Parameters:
+	net (RecurrentCompetitiveNet): the Brian2 net of which to build the graph
+	neurons_subsample (tuple): number of neurons to sample from excitatory and inhibitory population
+	subsample_attractors (bool): ad-hoc sampling from attractor A, attractor B, and non-specific population
+	seed (int): set seed to make the subsampling reproducible
+	remove_edges_threshold (float): remove edges whose weight is smaller than remove_edges_threshold from graph
+	output_filename (str): the name of the file to save to disk in os.getcwd()
+	
+	Returns:
+	networkx.DiGraph: The graph build from net
+	
+	"""
 	import os
 	
 	# ------ Subsample graph nodes
 	if neurons_subsample:
-		e_neurons_subsample = np.clip( neurons_subsample[ 0 ], 0, len( net.E ) )
-		i_neurons_subsample = np.clip( neurons_subsample[ 1 ], 0, len( net.I ) )
+		e_neurons_subsample = np.clip( neurons_subsample[ 0 ], 0, len( rcn.E ) )
+		i_neurons_subsample = np.clip( neurons_subsample[ 1 ], 0, len( rcn.I ) )
 		
-		if seed:
-			np.random.seed( seed )
+		rng = np.random.RandomState( seed )
 		if subsample_attractors:
 			assert e_neurons_subsample % 3 == 0
 			e_neurons = np.concatenate(
-					[ np.random.choice( range( 0, 64 ), int( e_neurons_subsample / 3 ), replace=False ),
-					  np.random.choice( range( 100, 164 ), int( e_neurons_subsample / 3 ), replace=False ),
-					  np.random.choice( range( 164, 256 ), int( e_neurons_subsample / 3 ), replace=False ) ]
+					[ rng.choice( range( 0, 64 ), int( e_neurons_subsample / 3 ), replace=False ),
+					  rng.choice( range( 100, 164 ), int( e_neurons_subsample / 3 ), replace=False ),
+					  rng.choice( range( 164, 256 ), int( e_neurons_subsample / 3 ), replace=False ) ]
 					)
-			i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
+			i_neurons = rng.choice( range( 0, len( rcn.I ) ), i_neurons_subsample, replace=False )
 		else:
-			e_neurons = np.random.choice( range( 0, len( net.E ) ), e_neurons_subsample, replace=False )
-			i_neurons = np.random.choice( range( 0, len( net.I ) ), i_neurons_subsample, replace=False )
+			e_neurons = rng.choice( range( 0, len( rcn.E ) ), e_neurons_subsample, replace=False )
+			i_neurons = rng.choice( range( 0, len( rcn.I ) ), i_neurons_subsample, replace=False )
 	else:
-		e_neurons = np.array( range( 0, len( net.E ) ) )
-		i_neurons = np.array( range( 0, len( net.I ) ) )
+		e_neurons = np.array( range( 0, len( rcn.E ) ) )
+		i_neurons = np.array( range( 0, len( rcn.I ) ) )
 	
 	# ------- Make sure that there are no duplicates in neurons subsample or Brian2 indexing runs into a bug
 	assert len( np.unique( e_neurons ) ) == len( e_neurons ) or len( np.unique( i_neurons ) ) == len( i_neurons )
-	e2e_edges_pre = net.E_E.i[ e_neurons, e_neurons ].tolist()
-	e2e_edges_post = net.E_E.j[ e_neurons, e_neurons ].tolist()
-	e2e_edge_weights = net.E_E.w_[ e_neurons, e_neurons ].tolist()
-	i2e_edges_pre = net.I_E.i[ i_neurons, e_neurons ].tolist()
-	i2e_edges_post = net.I_E.j[ i_neurons, e_neurons ].tolist()
-	i2e_edge_weights = net.I_E.w_[ i_neurons, e_neurons ].tolist()
+	e2e_edges_pre = rcn.E_E.i[ e_neurons, e_neurons ].tolist()
+	e2e_edges_post = rcn.E_E.j[ e_neurons, e_neurons ].tolist()
+	e2e_edge_weights = rcn.E_E.w_[ e_neurons, e_neurons ].tolist()
+	i2e_edges_pre = rcn.I_E.i[ i_neurons, e_neurons ].tolist()
+	i2e_edges_post = rcn.I_E.j[ i_neurons, e_neurons ].tolist()
+	i2e_edge_weights = rcn.I_E.w_[ i_neurons, e_neurons ].tolist()
 	
 	if remove_edges_threshold is not None:
 		e2e_edges_pre = [ k for i, k in enumerate( e2e_edges_pre ) if
@@ -76,7 +89,7 @@ def rcn2nx( net, neurons_subsample=None, subsample_attractors=False, seed=None,
 	for i, j, w in zip( i2e_edges_pre, i2e_edges_post, i2e_edge_weights ):
 		g.add_edge( f'i_{i}', f'e_{j}', weight=w )
 	
-	#  GraphML does not support list or any non-primitive type
+	# GraphML does not support list or any non-primitive type
 	if output_filename:
 		nx.write_graphml( g, f'{os.getcwd()}/{output_filename}.graphml' )
 	
@@ -89,7 +102,25 @@ def rcn2nx( net, neurons_subsample=None, subsample_attractors=False, seed=None,
 def nx2pyvis( input, notebook=False, output_filename='graph',
               scale_by='',
               neural_populations=None,
+              synapse_types=None,
               open_output=False, show_buttons=False, only_physics_buttons=False, window_size=(1000, 1000) ):
+	"""Given an instance of a Networx.Graph builds the corresponding PyVis graph for display purposes.
+
+	Parameters:
+	input (nx.Graph or str): the NetworkX graph to convert to PyVis or the path to a GraphML file to convert
+	output_filename (str): the name of the file HTML to save to disk in os.getcwd()
+	TODO - scale_by (str): the node parameter whose value is used to visually scale the nodes
+	TODO - neural_population (): which neural populations to include
+	TODO - synapse_types (): which synapse types to include
+	open_output (bool): set to True to open the HTML file in the default browser
+	show_buttons (bool): set to true to display the PyVis configuration UI in the HTML
+	only_physics_buttons (bool): set to true to display the physics PyVis configuration UI in the HTML
+	windows_size (tuple): the width and height of the window to display the graph in the HTML
+
+	Returns:
+	None
+
+	"""
 	from pyvis import network as net
 	
 	import pathlib
@@ -178,10 +209,17 @@ def nx2pyvis( input, notebook=False, output_filename='graph',
 # TODO measure net exhitation between attractors
 # TODO measure connectedness within each attractor
 
-"""Colour each attractor in a different colour."""
-
 
 def colour_by_attractor( g ):
+	"""Compute and store a different colour for each attractor in the NetworkX graph.
+	
+	Parameters:
+	g (networkx.Graph): the graph for which to compute the attractor colour
+	
+	Returns:
+	None
+	
+	"""
 	import cmasher as cmr
 	
 	num_attractors = len( set( nx.get_node_attributes( g, 'attractor' ).values() ) )
@@ -192,12 +230,23 @@ def colour_by_attractor( g ):
 		g.nodes[ n ][ 'color' ] = f'rgba({col[ 0 ]},{col[ 1 ]},{col[ 2 ]},1)'
 
 
-"""Compute the attractors and store the information in the nodes by finding which weakly-connected component they belong
-to.
-Should work correctly for RCN when edges s.t. w(e) < w_max are removed from the graph."""
-
-
 def tag_weakly_connected_components( g ):
+	"""Compute the attractors in the NetworkX graph and store the information in the nodes by finding which
+	weakly-connected component they belong to.
+	A directed graph is weakly connected if and only if the graph is connected when the direction of the edge between
+	nodes is ignored.  A connected graph is graph that is connected in the sense of a topological space, i.e.,
+	there is a path from any point to any other point in the graph.
+	
+	Should correctly identify attractors for RCN when edges s.t. w(e) < w_max have previously been removed from the
+	graph.
+	
+	Parameters:
+	g (networkx.Graph): the graph for which to compute the attractors
+	
+	Returns:
+	None
+	
+	"""
 	idx_components = { n: i for i, node_set in enumerate( nx.weakly_connected_components( g ) ) for n in node_set if
 	                   'e_' in n }
 	for n, i in idx_components.items():
@@ -205,6 +254,20 @@ def tag_weakly_connected_components( g ):
 
 
 def tag_attracting_components( g ):
+	"""Compute the attracting components in the NetworkX graph and store the information in the nodes.
+	
+	An attracting component in a directed graph G is a strongly connected component with the property that a random
+	walker on the graph will never leave the component, once it enters the component.  The nodes in attracting
+	components can also be thought of as recurrent nodes. If a random walker enters the attractor containing the node,
+	then the node will be visited infinitely often.
+	
+	Parameters:
+	g (networkx.Graph): the graph for which to compute the attracting components
+	
+	Returns:
+	None
+	
+	"""
 	from networkx.algorithms.components import is_attracting_component
 	
 	attracting_map = { }
@@ -221,46 +284,73 @@ def tag_attracting_components( g ):
 	nx.set_node_attributes( g, attracting_map )
 
 
-"""Compute what fraction of inhibitory cells connected to each attractor and the number of these connections to
-quantify the degree of inhibition of each attractor.
-For ex. attractor_inhibition_amount={0: 5, 1: 4} means that attractor 0 is receiving input from inhibitory cells via
-5 connections, and attractor 1 via 4.
-"""
-
-
-def attractor_inhibition( input ):
-	from networkx.algorithms.centrality import group_in_degree_centrality
+def attractor_inhibition( input, normalise=False ):
+	"""Compute the number of inhibitory connections incident to each attractor in the NetworkX graph.
+	This should be useful to quantify the degree of inhibition of each attractor.
+	For ex. attractor_inhibition_amount={0: 5, 1: 4} means that attractor 0 is receiving input from inhibitory cells
+	via 5 connections, and attractor 1 via 4.
+	
+	Parameters:
+	input (networkx.Graph or RecurrentCompetitiveNet): the graph or the RCN for which to compute the attractors'
+	inhibition amount
+	normalise (bool): if True the inhibition value is normalised by the total number of inhibitory connections
+	
+	Returns:
+	attractor_inhibition_amount (dict): dictionary of attractors with the amount of inhibition as the value
+	
+	"""
 	from helper_functions.recurrent_competitive_network import RecurrentCompetitiveNet
 	
 	if not isinstance( input, nx.Graph ) and isinstance( input, RecurrentCompetitiveNet ):
-		input = rcn2nx( input )
+		g = rcn2nx( input )
+	elif isinstance( input, nx.Graph ):
+		g = input
+	else:
+		raise ValueError( 'input must be of type nx.Graph or RecurrentCompetitiveNet' )
+	
+	if normalise:
+		norm = len( [ e for e in g.edges if 'i_' in e[ 0 ] ] )
+	else:
+		norm = 1
+	print( norm )
 	
 	attractor_inhibition_amount = { }
 	
-	for i in set( nx.get_node_attributes( input, 'attractor' ).values() ):
-		attractor_nodes = [ n for n, v in input.nodes( data=True ) if 'e_' in n and v[ 'attractor' ] == i ]
-		inhibitory_nodes = [ n for n in input.nodes if 'i_' in n ]
-		subgraph = input.subgraph( attractor_nodes + inhibitory_nodes )
+	for i in set( nx.get_node_attributes( g, 'attractor' ).values() ):
+		attractor_nodes = [ n for n, v in g.nodes( data=True ) if 'e_' in n and v[ 'attractor' ] == i ]
+		inhibitory_nodes = [ n for n in g.nodes if 'i_' in n ]
+		subgraph = g.subgraph( attractor_nodes + inhibitory_nodes )
 		
 		attractor_inhibition_amount[ i ] = \
-			len( [ e for e in input.edges if e[ 0 ] in inhibitory_nodes and e[ 1 ] in attractor_nodes ] )
+			len( [ e for e in subgraph.edges if e[ 0 ] in inhibitory_nodes and e[ 1 ] in attractor_nodes ] ) / norm
 	
 	return attractor_inhibition_amount
 
 
-"""Computes the average connectivity within each attractor.
-The average connectivity of a graph G is the average of local node connectivity over all pairs of nodes of
-G.  Local node connectivity for two non adjacent nodes s and t is the minimum number of nodes that must be removed (
-along with their incident edges) to disconnect them.
-"""
-
-
 def attractor_connectivity( input ):
+	"""Computes the average node connectivity within each attractor in the NetworkX graph.
+	This should be useful to quantify the amount of self-excitation each attractor has.
+	The average connectivity of a graph G is the average of local node connectivity over all pairs of nodes of G.
+	Local node connectivity for two non adjacent nodes s and t is the minimum number of nodes that must be removed (
+	along with their incident edges) to disconnect them.
+	
+	Parameters:
+	input (networkx.Graph or RecurrentCompetitiveNet): the graph or the RCN for which to compute the attractors'
+	connectivity
+	
+	Returns:
+	attractor_connectivity_amount (dict): dictionary of attractors with the average node connectivity as the value
+	
+"""
 	from networkx.algorithms.connectivity.connectivity import average_node_connectivity
 	from helper_functions.recurrent_competitive_network import RecurrentCompetitiveNet
 	
 	if not isinstance( input, nx.Graph ) and isinstance( input, RecurrentCompetitiveNet ):
-		input = rcn2nx( input )
+		g = rcn2nx( input )
+	elif isinstance( input, nx.Graph ):
+		g = input
+	else:
+		raise ValueError( 'input must be of type nx.Graph or RecurrentCompetitiveNet' )
 	
 	attractor_connectivity_amount = { }
 	
@@ -273,7 +363,19 @@ def attractor_connectivity( input ):
 	return attractor_connectivity_amount
 
 
-def save_graph_results( comment='', folder='interesting_graph_results', additional_files=None ):
+# TODO save generated statistics too
+def save_graph_results( folder='interesting_graph_results', additional_files=None, comment='' ):
+	"""Saves files resulting from graphing in a folder
+
+	Parameters:
+	folder (str): the base folder under which results will be saved
+	additional_files (list): any additional files to move along with the default ones
+	TODO - comment (str): any comments to add to the folder
+
+	Returns:
+	None
+
+"""
 	import shutil
 	import datetime
 	import os
--- tt_tests/network_viz.py
+++ tt_tests/network_viz.py
@@ -2,7 +2,6 @@
 import os
 
 import numpy as np
-import pandas as pd
 
 from brian2 import second, prefs, NeuronGroup, Synapses
 
