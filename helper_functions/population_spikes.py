9a3540f9eab5d942a6243e680ad614da385d460f
WillianSG <w.soares.girao@rug.nl>
2022-10-06 11:47:49 +0200

    running grid search on BA and ie_w amount using gompertz curve

--- helper_functions/population_spikes.py
+++ helper_functions/population_spikes.py
@@ -65,4 +65,43 @@ def count_ps(rcn, attractors: list, time_window: list, spk_sync_thr: float):
 
 	return attractors_ps_counts
 
+def count_ps_v2(rcn, attractors: list, time_window: list, spk_sync_thr: float):
+
+	from other import find_ps
+
+	attractors_ps_counts = {}
+
+	us_neurs_with_input, sim_t_array, U, tau_f = rcn.get_u_traces_from_pattern_neurons()
+
+	for i in range(0, len(attractors)):
+
+		attractors_ps_counts[attractors[i][0]] = {'triggered': 0, 'spontaneous': 0}
+
+		x, y, y_smooth, pss = find_ps(rcn.net_sim_data_path, sim_t_array[-1], attractors[i])
+
+		_last_trg_ps = 0
+		_last_spt_ps = 0
+
+		for j in range(0, len(x)):
+
+			if x[j] >= time_window[i][0] and x[j] <= time_window[i][1]:
+
+				if y[j] >= spk_sync_thr and x[j] > _last_trg_ps:
+
+					attractors_ps_counts[attractors[i][0]]['triggered'] += 1
+
+					_last_trg_ps = x[j] + 0.15
+
+			else:
+
+				if (x[j] < time_window[i][0] or x[j] > time_window[i][1]) and (x[j] < time_window[i][2][0] or x[j] > time_window[i][2][1]):
+
+					if y[j] >= spk_sync_thr and x[j] > _last_spt_ps:
+
+						attractors_ps_counts[attractors[i][0]]['spontaneous'] += 1
+
+						_last_spt_ps = x[j] + 0.15
+
+	return attractors_ps_counts
+
 	
