085407a375c59393d223a144a3453e94cc35efab
Thomas Tiotto <thomas.tiotto@outlook.com>
2023-11-07 11:34:29 +0100

    - RCN_energy_usage.py is parallel

--- RCN/RCN_energy_usage.py
+++ RCN/RCN_energy_usage.py
@@ -2,41 +2,44 @@ import ast
 import os
 from collections import defaultdict
 from datetime import datetime
+from functools import partial
 from statistics import mean
 
 import pandas as pd
 import numpy as np
 from random import randrange
 
+import pathos
+import tqdm_pathos
 from matplotlib import pyplot as plt
 
 from helper_functions.recurrent_competitive_network import run_rcn
 
 
 def run_rcn_and_compute_frequency(params, num_attractors=2, num_cues=2):
-    print('-----------------------------------')
-    print('Chosen parameters:')
-    print(f'\tScore: {params["score"]}')
-    print(f'\tAccuracy: {params["accuracy"]}')
-    print(f'\tRecall: {params["recall"]}')
+    # print('-----------------------------------')
+    # print('Chosen parameters:')
+    # print(f'\tScore: {params["score"]}')
+    # print(f'\tAccuracy: {params["accuracy"]}')
+    # print(f'\tRecall: {params["recall"]}')
     params = ast.literal_eval(params['params'])
-    print(f'\t{params}')
+    # print(f'\t{params}')
 
     params['num_attractors'] = num_attractors
     params['num_cues'] = num_cues
     params['recall_time'] = 4
 
     rcn, returned_params = run_rcn(params, show_plot=False,
-                                   progressbar=True, low_memory=False,
+                                   progressbar=False, low_memory=False,
                                    attractor_conflict_resolution="3", return_complete_stats=True,
                                    seed_init=seed)
 
-    print('\tPS frequency')
+    # print('\tPS frequency')
     ps_frequencies = {}
     for k, v in returned_params['ps_counts'].items():
         ps_freq = len(v['triggered']) / v['cued_time']
         ps_frequencies[k] = ps_freq
-        print(f"\t\t{k}: {ps_freq} Hz")
+        # print(f"\t\t{k}: {ps_freq} Hz")
 
     def get_spikes_frequency(spk_mon_ts):
         spikes_cues = defaultdict(list)
@@ -51,11 +54,11 @@ def run_rcn_and_compute_frequency(params, num_attractors=2, num_cues=2):
         # print('\t\tCued frequency')
         for k, v in spikes_cues.items():
             spikes_cues[k] = np.mean(v)
-            print(f"\t\t\t{k}: {spikes_cues[k]} Hz")
+            # print(f"\t\t\t{k}: {spikes_cues[k]} Hz")
         # print('\t\tRecall frequency')
         for k, v in spikes_recall.items():
             spikes_recall[k] = np.mean(v)
-            print(f"\t\t\t{k}: {spikes_recall[k]} Hz")
+            # print(f"\t\t\t{k}: {spikes_recall[k]} Hz")
 
         return spikes_cues, spikes_recall
 
@@ -81,35 +84,35 @@ num_cues = 100
 seed = randrange(1000000)
 # load results from RCN Bayesian sweep
 df = pd.read_csv("RESULTS/BAYESIAN_OPTIMISATION/R=0.5_BAYESIAN_SAVED_(2023-11-04_13-09-55)/results.csv")
-# original parameters
-# run_rcn_and_compute_frequency(pd.Series({'score': -1, 'accuracy': -1, 'recall': -1,
-#                                          'params': "{'attractor_size': 64, 'background_activity': 15, 'cue_length': 1, 'cue_percentage': 100, 'e_e_max_weight': 10, 'e_i_weight': 3, 'i_e_weight': 10, 'i_frequency': 20, 'network_size': 256, 'num_attractors': 4, 'num_cues': 10}"}),
-#                               num_attractors, num_cues)
+
 # sweep recalls in [0.1-1] with step 0.1
-recall_plot_list = []
-ps_frequencies_plot_list = []
-spikes_cues_plot_list_e = []
-spikes_recall_plot_list_e = []
-spikes_cues_plot_list_i = []
-spikes_recall_plot_list_i = []
-energy_cues_plot_list = []
-energy_recalls_plot_list = []
-accuracies_plot_list = []
 lower_lim = np.arange(0., 1.0, 0.1)
 upper_lim = np.arange(0.1, 1.1, 0.1)
 # lower_lim = [0.8, 0.9]
 # upper_lim = [0.9, 1.0]
+
+recall_plot_list = []
+param_grid_pool = []
 for r1, r2 in zip(lower_lim, upper_lim):
     queried_param = df.query(f'accuracy == 1 & recall > {r1} & recall <= {r2}').iloc[0]
+    param_grid_pool.append(queried_param)
     recall_plot_list.append(queried_param['recall'])
-    ps_frequencies, freq_cues_e, freq_recall_e, freq_cues_i, freq_recall_i, returned_params = run_rcn_and_compute_frequency(
-        queried_param, num_attractors, num_cues)
-    ps_frequencies_plot_list.append(mean(ps_frequencies.values()))
-    spikes_cues_plot_list_e.append(mean(freq_cues_e.values()))
-    spikes_recall_plot_list_e.append(mean(freq_recall_e.values()))
-    spikes_cues_plot_list_i.append(mean(freq_cues_i.values()))
-    spikes_recall_plot_list_i.append(mean(freq_recall_i.values()))
-    accuracies_plot_list.append(returned_params)
+
+results = tqdm_pathos.map(
+    partial(
+        run_rcn_and_compute_frequency,
+        num_attractors=num_attractors,
+        num_cues=num_cues
+    ),
+    param_grid_pool,
+    n_cpus=pathos.multiprocessing.cpu_count(),
+)
+
+ps_frequencies_plot_list = [mean(p[0].values()) for p in results]
+spikes_cues_plot_list_e = [mean(p[1].values()) for p in results]
+spikes_recall_plot_list_e = [mean(p[2].values()) for p in results]
+spikes_cues_plot_list_i = [mean(p[3].values()) for p in results]
+spikes_recall_plot_list_i = [mean(p[4].values()) for p in results]
 
 num_E_neurons = num_attractors * ast.literal_eval(queried_param['params'])['attractor_size']
 num_I_neurons = ast.literal_eval(queried_param['params'])['network_size'] // 4
